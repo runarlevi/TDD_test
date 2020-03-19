321592699529f80253ad52afae1ae54e5dd40951
runarlevi <runar18@ru.is>
2020-03-19 15:02:15 +0000

    Supports divisible by three

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,3 +3,5 @@ import tests
 def test_returns_number():
     assert tests.fizzbuzz(2) == 2
 
+def test_returns_fizz_when_divisible_by_three():
+    assert tests.fizzbuzz(3) == "Fizz"
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 
-
 def fizzbuzz(input):
+    if input % 3 == 0:
+        return "Fizz"
     return input
